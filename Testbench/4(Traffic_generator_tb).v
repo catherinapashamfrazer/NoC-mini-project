7645b597856f669755ae8f0e1f2d28d1bdde03b6
catherinapashamfrazer <catherinapashamfrazer@gmail.com>
2026-06-09 11:56:01 +0530

    Create 4(Traffic_generator_tb).v

---
+++ Testbench/4(Traffic_generator_tb).v
@@ -0,0 +1,26 @@
+module traffic_generator_tb;
+reg clk;
+reg reset;
+wire [7:0] packet_out;
+traffic_generator uut(
+    .clk(clk),
+    .reset(reset),
+    .packet_out(packet_out)
+);
+always #5 clk = ~clk;
+initial
+begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0,traffic_generator_tb);
+    clk = 0;
+    reset = 1;
+    #10;
+    reset = 0;
+    #100;
+    $finish;
+end
+initial
+begin
+    $monitor("Time=%0t Packet=%b",$time,packet_out);
+end
+endmodule
